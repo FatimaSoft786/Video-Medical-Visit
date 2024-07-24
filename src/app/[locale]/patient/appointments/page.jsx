9e76f80f854d13e77a2a563d01eb064e1fab6670
Hassam Bhutta <126439037+adilsarfraz02@users.noreply.github.com>
2024-07-24 13:56:48 +0500

    Update Appointment Page - Tabs

--- src/app/[locale]/patient/appointments/page.jsx
+++ src/app/[locale]/patient/appointments/page.jsx
@@ -4,10 +4,78 @@ import React, { useEffect, useState } from "react";
 import MedicalSection from "../components/MedicalSection";
 import ConfirmDoctor from "../components/ConfirmedDoctor";
 import { getUserSession } from "@/utils/session";
+import LoadingSkeleton from "../../doctor/appointments/LoadingSkeleton";
 
-const Page = () => {
-  const [appointments, setAppointments] = useState([]);
-  const [loading, setLoading] = useState(true);
+const Tabs = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div className="container mx-auto py-8">
+      <div className="flex border-b my-6">
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={`items-center text-center px-8 py-3 focus:outline-none ${
+              activeTab === index
+                ? "border-b-3 font-bold border-dark-blue text-dark-blue"
+                : "text-black"
+            }`}
+            onClick={() => setActiveTab(index)}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className="mt-4">{tabs[activeTab] && tabs[activeTab].content}</div>
+    </div>
+  );
+};
+
+
+
+const ScheduledAppointments = ({ appointments, isLoading }) => {
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <LoadingSkeleton key={index} />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      {appointments.map((appointment, index) => (
+        <ConfirmDoctor key={index} appointment={appointment} />
+      ))}
+    </div>
+  );
+};
+
+const DoneAppointments = ({ appointments, isLoading }) => {
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <LoadingSkeleton key={index} />
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      {appointments.map((appointment, index) => (
+        <ConfirmDoctor key={index} appointment={appointment} isDone />
+      ))}
+    </div>
+  );
+};
+const AppointmentTabsPage = () => {
+  const [appointmentsDone, setAppointmentsDone] = useState([]);
+  const [appointmentsScheduled, setAppointmentsScheduled] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const { user } = getUserSession();
   const patientId = user.user_details._id;
 
@@ -25,8 +93,15 @@ const Page = () => {
         }
       );
       const data = await response.json();
-      setAppointments(data.appointments);
-      setLoading(false);
+      const done = data.appointments.filter(
+        (appointment) => appointment.payment_status === "Paid"
+      );
+      const scheduled = data.appointments.filter(
+        (appointment) => appointment.appointment_status === "waiting"
+      );
+      setAppointmentsScheduled(scheduled);
+      setAppointmentsDone(done);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error fetching appointments:", error);
     }
@@ -36,44 +111,29 @@ const Page = () => {
     fetchAppointments();
   }, [patientId]);
 
-  const DoctorCardSkeleton = () => (
-    <div className="animate-pulse bg-gray-200 rounded-lg p-4">
-      <div className="h-32 bg-gray-300 rounded mb-4"></div>
-      <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-    </div>
-  );
-
-  if (loading) {
-    return (
-      <div className="overflow-hidden">
-        <title>Appointment | A Doctor's Appointment</title>
-        <MedicalSection />
-        <div className="w-full grid container mx-auto grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 mt-8 p-8">
-          <DoctorCardSkeleton />
-          <DoctorCardSkeleton />
-          <DoctorCardSkeleton />
-          <DoctorCardSkeleton />
-        </div>
-      </div>
-    );
-  }
+  const tabsData = [
+    {
+      label: "Scheduled Appointments",
+      content: (
+        <ScheduledAppointments appointments={appointmentsScheduled} isLoading={isLoading} />
+      ),
+    },
+    {
+      label: "Done Appointments",
+      content: (
+        <DoneAppointments appointments={appointmentsDone} isLoading={isLoading} />
+      ),
+    },
+  ];
 
   return (
     <div className="overflow-hidden">
       <title>Appointment | A Doctor's Appointment</title>
       <MedicalSection />
-      <div className="grid grid-cols-1 container mx-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-8">
-        {appointments.map((appointment) => (
-          <ConfirmDoctor
-            key={appointment._id}
-            appointment={appointment}
-            patientId={patientId}
-            onAppointmentChange={fetchAppointments}
-          />
-        ))}
-      </div>
+      <Tabs tabs={tabsData} />
     </div>
   );
 };
 
-export default Page;
+export default AppointmentTabsPage;
+
